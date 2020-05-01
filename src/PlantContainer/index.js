c14f6a81efe476c114db69ae2f995ba1320a8fd6
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-05-01 13:30:14 -0500

    "React app is set up and App.js renders PlantContainer now"

---
+++ src/PlantContainer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+export default class PlantContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <h3> Welcome to Plantery </h3>
+      </React.Fragment>
+    )
+  }
+}
